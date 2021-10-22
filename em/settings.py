316e4c6e4a1fbb7383c544c54906ef37f78ca5de
toufic118284 <toufic064@gmail.com>
2021-10-22 23:39:51 +0600

    The assets file was created

--- em/settings.py
+++ em/settings.py
@@ -126,6 +126,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATIC_ROOT = 'assets/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
